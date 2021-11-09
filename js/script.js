e1f631b3f7413f98465e956c1026ea9b6d855ff3
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-09 17:16:56 +0100

    soluzione 2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 // alert ("ciaoooooooo")
 
-//============== ins variabili 
+//============== ins variabili soluzione 1
 
 const ul = document.querySelector(".list");
 let x;
@@ -22,4 +22,36 @@ for (let i = 1; i <= 50; i++) {
 ul.innerHTML += `<li class="box ${x}">${x}</li>`;
 console.log(x);
 }
+
+//============== ins variabili soluzione 2 
+
+// const ul = document.querySelector(".list");
+// let li;
+// let x;
+
+// for (let i = 1; i <= 50; i++) {
+//     li = document.createElement("li");
+//     li.className = 'box';
+//     ul.append(li);
+//     if(i % 15 == 0){
+//         x = 'fizzbuzz'; 
+//         li.classList.add(x)      
+//     }else if(i % 3 == 0){
+//         x = 'fizz';
+//         li.classList.add(x)
+//     }else if(i % 5 == 0){
+//         x = 'buzz';
+//         li.classList.add(x)
+//     }else{
+//         x = i;
+//     }
+
+//     li.innerHTML = x;
+    
+// // =============output 
+// // ul.innerHTML += `<li class="box ${x}">${x}</li>`;
+
+
+// console.log(x);
+// }
 
